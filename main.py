ecfd9dee26df26b9e7fa846cff8ce48897199f62
Yassine Lazreg <yessinelazraj@gmail.com>
2022-12-05 17:32:34 +0100

    commit

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
 import os
 from google.cloud import texttospeech_v1
 
+
+# https://cloud.google.com/text-to-speech/docs/libraries
 # global variable for the access-key
 # path has to be adjusted
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"/Users/yassinelazreg/PycharmProjects/pythonProject5/venv/text2speech-363215-594ba12ae6b8.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"addPath"
 
-# Instantiates a client
-client = texttospeech_v1.TextToSpeechClient()
+# create a client instance
+c = texttospeech_v1.TextToSpeechClient()
 
 
-# Set the text input to be synthesized
+# choose text input that will be turned into speech
 sentence = ''
-synthesis_input = texttospeech_v1.SynthesisInput(text=sentence)
+text = texttospeech_v1.SynthesisInput(text=sentence)
 
-# Select the voice
-voice = texttospeech_v1.VoiceSelectionParams(
+# Select the voice and the language
+speaker = texttospeech_v1.VoiceSelectionParams(
     name='en-US-Wavenet-A', language_code="en-US"
 )
 
-# Select the type of audio file you want returned and set pitch and speaking rate
-audio_config = texttospeech_v1.AudioConfig(
+# choose file encoding type, speech rate, and pitch level
+params = texttospeech_v1.AudioConfig(
     # https://cloud.google.com/text-to-speech/docs/reference/rpc/google.cloud.texttospeech.v1#audioencoding
     audio_encoding=texttospeech_v1.AudioEncoding.MP3,
     #speaking_rate=1.5    # increased speach rate by 50%
     pitch=-6              # decreased pitch by 6 semitones
 )
 
-# Perform the text-to-speech request on the text input with the selected
-# voice parameters and audio file type
-response = client.synthesize_speech(
-    input=synthesis_input, voice=voice, audio_config=audio_config
+# make TTS request with the chosen text input and speech parameters
+speech = c.synthesize_speech(
+    input=text, voice=speaker, audio_config=params
 )
 
 
 # The response's audio_content is binary.
 with open(r"controlQuetsion.mp3", "wb") as out:
-    # Write the response to the output file.
-    out.write(response.audio_content)
-    print('Audio content written to file "voiceC-male-normal.mp3"')
+    # save the generated speech sample
+    out.write(speech.audio_content)
